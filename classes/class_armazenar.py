3e1044924fa993000661cdd62e640db45627d7f1
Jose Mateus <jmateussilva044@gmail.com>
2023-10-13 10:44:12 -0300

    criei uma nova pasta para fzr as classes

---
+++ classes/class_armazenar.py
@@ -0,0 +1,11 @@
+class Armazenar():
+    def __init__(self):
+        self._dados = dict()
+    
+    def armazenar(self, pessoa):
+        self._dados[pessoa._cpf] = pessoa
+    
+
+    def exibir_pessoa(self):
+        for i, v in self._dados:
+            print(f'{i} - {v._nome} - {v._email}')
