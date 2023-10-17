c8a6fe7c27bb049504997b993bd14fd3ac260bde
Raileal <railealyt@gmail.com>
2023-10-17 16:39:23 -0300

    Fiz oque o professor disse e adicionai uma nova tela em branco que precisa de funcionalidade, cineplus2.0, tela de inicio melhorada e atualizada

--- funcionartelamain.py
+++ funcionartelamain.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
 from tela_main_ui import *
 from TELA_CADASTRO_ui import *
-from tela_login_ui import *
+from tela_semnome_ui import *
 from classes.class_armazenar import *
 from classes.class_pessoa import *
 from classes.funcoes_aux import *
@@ -21,40 +21,34 @@ class Main(QtWidgets.QWidget):
         self.stack0 =QtWidgets.QMainWindow()
         self.stack1 =QtWidgets.QMainWindow()
         self.stack2 =QtWidgets.QMainWindow()
-        
+         
         #Fazendo a chamada de cada uma individualmente
         self.tela_main_ui=Ui_Dialog()
         self.tela_main_ui.setupUi(self.stack0)
 
-        self.TELA_CADASTRO_ui=Cadastrar()
+        self.TELA_CADASTRO_ui = Cadastrar()
         self.TELA_CADASTRO_ui.setupUi(self.stack1)
 
-        self.tela_login_ui = Login()
-        self.tela_login_ui.setupUi(self.stack2)
+        self.tela_semnome_ui = AposLogin()
+        self.tela_semnome_ui.setupUi(self.stack2)
         
         #Fazendo Funcionar
         self.QtStack.addWidget(self.stack0)
         self.QtStack.addWidget(self.stack1)
         self.QtStack.addWidget(self.stack2)
 
-class Main(QMainWindow, Main):
+class Ui_Main(QMainWindow, Main):
     def __init__(self):
         super(Main, self).__init__(None)
         self.setupUi(self)
         
         #Tela main
         self.tela_main_ui.pushButton_3.clicked.connect(self.fecharAplicacao) #FECHA O APP PELO BOTAO
-        self.tela_main_ui.pushButton.clicked.connect(self.abrirTelaCadastro) # TELA CADASTRO
-        self.tela_main_ui.pushButton_2.clicked.connect(self.abrirLogin) #TELA LOGIN 
-        
-        #tela Login
-        self.tela_login_ui.pushButton_3.clicked.connect(self.VoltarMain) #Volta Menu Inicial
-        self.tela_login_ui.pushButton_2.clicked.connect(self.abrirTelaCadastro) #Abre direto na tela cadastro
-        self.tela_login_ui.pushButton.clicked.connect(self.botao_ok) #abre a telinha de confirmação
+        self.tela_main_ui.pushButton_4.clicked.connect(self.abrirTelaCadastro) # TELA CADASTRO
+        self.tela_main_ui.pushButton.clicked.connect(self.botao_ok) #TELA LOGIN 
         
         #tela Cadastro
         self.TELA_CADASTRO_ui.pushButton_3.clicked.connect(self.VoltarMain) #Volta Menu Inicial
-        self.TELA_CADASTRO_ui.pushButton_2.clicked.connect(self.abrirLogin) #Entra Direto no Login
         self.TELA_CADASTRO_ui.pushButton.clicked.connect(self.botao_Cadastra) #abre a telinha de confirmação de cadastro
         
     def botao_Cadastra(self):
@@ -86,19 +80,25 @@ class Main(QMainWindow, Main):
             self.QtStack.setCurrentIndex(0)
             
     
-    def botao_ok(self): #responsavel por abrir a tela de confirmação
-        cpf = self.tela_login_ui.lineEdit.text()
-        senha = self.tela_login_ui.lineEdit_2.text()
+    def botao_ok(self): #responsavel por LOGAR
+        valid = False
+        cpf = self.tela_main_ui.lineEdit_2.text()
+        senha = self.tela_main_ui.lineEdit.text()
         if cpf == '' or senha == '':
             QtWidgets.QMessageBox.information(self, 'erro', 'Digite valores válidos.')
         
         elif dados.verificar_login(cpf, senha):
             QtWidgets.QMessageBox.information(self, 'login', 'login realizado com sucesso.')
-            self.tela_login_ui.lineEdit.setText('')
-            self.tela_login_ui.lineEdit_2.setText('')
+            valid = True
+            self.tela_main_ui.lineEdit_2.setText('')
+            self.tela_main_ui.lineEdit.setText('')
         else:
             QtWidgets.QMessageBox.information(self, 'Erro', 'Login ou senha invalido.')
-        
+            self.tela_main_ui.lineEdit_2.setText('')
+            self.tela_main_ui.lineEdit.setText('')
+            
+        if valid:
+            self.QtStack.setCurrentIndex(2)
 
     def botao_confirmacao(self): #responsavel por abrir a tela de confirmação de cadastro
         QtWidgets.QMessageBox.information(self, 'cadastro', 'pessoa cadastrada com sucesso.')
@@ -111,12 +111,12 @@ class Main(QMainWindow, Main):
     
     def abrirTelaCadastro(self):
         self.QtStack.setCurrentIndex(1)
-    
-    def abrirLogin(self):
+   
+    def abrirTelaAposLogin(self):
         self.QtStack.setCurrentIndex(2)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    show_main = Main()
+    show_main = Ui_Main()
     sys.exit(app.exec_())
 
